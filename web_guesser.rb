8cd2024bfd492b66a65b86d35b540e1c9cc57dee
ZooBot Mars <sasa345@yahoo.com>
2015-10-07 17:52:25 -0600

    I3: working web guesser

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,8 +2,30 @@ ARGV << '-o' << '127.0.0.1'  # work around localhost issue
 require 'sinatra'
 require 'sinatra/reloader'
 
-random_num = rand(100)
+SECRET_NUM = rand(100)
+
+def check_guess(guess_num)
+  difference = (SECRET_NUM - guess_num).abs
+  if guess_num > SECRET_NUM
+    if difference > 5
+      message = "Way too high!"
+    else
+      message = "Too high"
+    end
+  elsif guess_num < SECRET_NUM
+    if difference > 5
+      message = "Way too low!"
+    else
+      message = "Too low!"
+    end
+  else
+    message = "You got it! The secret number is #{SECRET_NUM}"
+  end
+  message
+end
 
 get '/' do
-  "THE SECRET NUMBER is #{random_num}"
+  guess_num = params["guess"].to_i
+  message = check_guess(guess_num)
+  erb :index, :locals => {:number => SECRET_NUM, :message => message}
 end
