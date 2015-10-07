6a17e7398ace9bd42297a71a2539de431af43773
ZooBot Mars <sasa345@yahoo.com>
2015-10-07 12:15:45 -0600

    I1: generates a random number

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,6 +1,9 @@
 ARGV << '-o' << '127.0.0.1'  # work around localhost issue
-
 require 'sinatra'
+require 'sinatra/reloader'
+
+random_num = rand(100)
+
 get '/' do
-  "Hello, World!"
+  "THE SECRET NUMBER is #{random_num}"
 end
